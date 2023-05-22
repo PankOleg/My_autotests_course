1a0bf5f8589746ee863f0c19f268eac647e601f9
Oleg <bigstan93@mail.ru>
2023-05-22 22:24:41 +0500

    Four task complete

---
+++ homework4/Домашка4.6.py
@@ -0,0 +1,36 @@
+# Напишите функцию, которая принимает кортеж num_tuple из 10 цифр num_tuple,
+# и возвращает строку этих чисел в виде номера телефона str_phone.
+# Например (Ввод --> Вывод) :
+# (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)  => "(123) 456-7890"
+
+def create_phone_number(num_tuple):
+    lst = []
+    str_phone = []
+    for number in num_tuple:
+        for i in str(number):
+            lst.append(i)
+        chap1 = str(lst[0:3:])
+        chap2 = str(lst[3:6:])
+        chap3 = str(lst[6::])
+        str_phone = f"({chap1[2:14:5]}) {chap2[2:14:5]}-{chap3[2:19:5]}"
+    return str_phone
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [
+    (1, 2, 3, 4, 5, 6, 7, 8, 9, 0),
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (0, 2, 3, 0, 5, 6, 0, 8, 9, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+]
+
+test_data = [
+    "(123) 456-7890", "(111) 111-1111", "(023) 056-0890", "(000) 000-0000"
+]
+
+for i, d in enumerate(data):
+    assert create_phone_number(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
